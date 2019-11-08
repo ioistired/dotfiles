312a45474d2563147bcf885839b0e66b6539e82b
Io Mintz <io@mintz.cc>
2019-11-08 16:34:38 +0000

    fix fish_prompt.fish

--- dotfiles/functions/fish_prompt.fish
+++ dotfiles/functions/fish_prompt.fish
@@ -1,1 +1,20 @@
-/home/io/.local/share/omf/themes/gitstatus/fish_prompt.fish
+# Defined in /tmp/fish.yJf3Tf/fish_prompt.fish @ line 2
+function fish_prompt --description 'Write out the prompt'
+	set -l last_status $status
+
+    # PWD
+    set_color $fish_color_cwd
+    echo -n (prompt_pwd)
+    set_color normal
+
+    __terlar_git_prompt
+    __fish_hg_prompt
+    echo
+
+    if not test $last_status -eq 0
+        set_color $fish_color_error
+    end
+
+    echo -n '➤ '
+    set_color normal
+end
