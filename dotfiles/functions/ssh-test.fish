48ee5559a88a986702e282c5fb5ce40f9a0140b7
Io Mintz <io@mintz.cc>
2019-11-11 17:18:32 +0000

    remove unused ssh funcs
    now I use KeepassXC as my SSH agent so I don't need these

--- dotfiles/functions/ssh-test.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /tmp/fish.rj8LBQ/ssh-test.fish @ line 2
-function ssh-test --description 'test whether the given key file is added to the ssh daemon' --argument file
-	command ssh-add -l | grep -q (ssh-keygen -lf $file)
-end
