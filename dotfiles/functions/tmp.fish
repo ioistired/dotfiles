e197da1929ca71f42f914afbe59c35616abe4c14
Io Mintz <io@mintz.cc>
2019-11-14 01:23:28 +0000

    function tmp: allow specifying a custom tmp dir

--- dotfiles/functions/tmp.fish
+++ dotfiles/functions/tmp.fish
@@ -1,9 +1,17 @@
-# Defined in /tmp/fish.XuVFDz/tmp.fish @ line 2
-function tmp
+# Defined in /tmp/fish.GFBatk/tmp.fish @ line 2
+function tmp --argument dir
 	if set -q _current_tmp_dir
 		cd $_current_tmp_dir
 		return 0
 	end
-	set -U _current_tmp_dir (mktemp -d)
+
+	if test -z $dir
+		set dir (mktemp -d)
+	else
+		set dir (realpath $dir)
+		mkdir -p $dir; or return $status
+	end
+
+	set -U _current_tmp_dir $dir
 	tmp
 end
