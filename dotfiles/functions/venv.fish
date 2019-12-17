6a28294da17b38467b310a94061389e6aaa8bd33
Io Mintz <io@mintz.cc>
2019-12-17 00:06:13 +0000

    function venv: don't install aioconsole
    It's not needed now that I run python3.8 on my machines, which includes an asyncio REPL

--- dotfiles/functions/venv.fish
+++ dotfiles/functions/venv.fish
@@ -1,11 +1,11 @@
-# Defined in /tmp/fish.41gxLn/venv.fish @ line 2
+# Defined in /tmp/fish.M4X2qm/venv.fish @ line 2
 function venv --argument dir
 	test -z $dir; and set -l dir .venv
 	if test ! -d $dir
 		set -l user_site_dir (python -m site --user-site)
 		python -m venv $dir
 		venv $dir
-		pip install --upgrade setuptools pip wheel import_expression aioconsole pythonpy-clone
+		pip install --upgrade setuptools pip wheel import_expression pythonpy-clone
 		cp $user_site_dir/sitecustomize.py (get-venv-site-packages)/
 		return 0
 	end
