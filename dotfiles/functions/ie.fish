0a30f35e5f4db4c0da118cb89a1388014a3e6d1c
Io Mintz <io@mintz.cc>
2020-04-21 06:46:42 +0000

    ie alias: add -q flag

--- dotfiles/functions/ie.fish
+++ dotfiles/functions/ie.fish
@@ -1,4 +1,4 @@
-# Defined in - @ line 1
+# Defined in /tmp/fish.aYJI6s/ie.fish @ line 2
 function ie --description 'alias ie import_expression'
-	import_expression  $argv;
+	import_expression -q $argv;
 end
