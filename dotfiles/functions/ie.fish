5ac9b6fe20d01527b7f191839480f07051268888
Io Mintz <io@mintz.cc>
2020-04-30 20:21:11 +0000

    update ie alias

--- dotfiles/functions/ie.fish
+++ dotfiles/functions/ie.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.aYJI6s/ie.fish @ line 2
+# Defined in /tmp/fish.FTmNdl/ie.fish @ line 2
 function ie --description 'alias ie import_expression'
-	import_expression -q $argv;
+	import-expression -q $argv;
 end
