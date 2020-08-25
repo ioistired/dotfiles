6e5ee2898ec20dea0861cfbf1f8740049422244f
Io Mintz <io@mintz.cc>
2020-08-25 05:35:45 +0000

    4nxci: use existing prod.keys file

---
+++ dotfiles/functions/4nxci.fish
@@ -0,0 +1,4 @@
+# Defined in /home/user/.config/fish/functions.local/4nxci.fish @ line 2
+function 4nxci --description 'alias 4nxci 4nxci -k ~/Downloads/nx-hax/keys.dat'
+	command 4nxci -k ~/Downloads/nx-hax/keys.dat $argv;
+end
