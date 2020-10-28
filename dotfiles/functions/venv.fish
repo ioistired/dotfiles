14a69bd1b7d8803b5c98831cf6d30f63ccc63ee5
Io Mintz <io@mintz.cc>
2020-10-28 23:58:09 +0000

    venv: always create fresh venvs

--- dotfiles/functions/venv.fish
+++ dotfiles/functions/venv.fish
@@ -1,7 +1,10 @@
-# Defined in /tmp/fish.M4X2qm/venv.fish @ line 2
+# Defined in /tmp/fish.I8m6yk/venv.fish @ line 2
 function venv --argument dir
 	test -z $dir; and set -l dir .venv
 	if test ! -d $dir
+		# don't base our new venv off of an existing one
+		test -n $VIRTUAL_ENV; and deactivate
+
 		set -l user_site_dir (python -m site --user-site)
 		python -m venv $dir
 		venv $dir
