e0f30e7c5b33e92f193107024265bdd8cbb94d7c
io <gie9ohbeixah@paperboats.net>
2021-05-13 18:40:29 +0000

    move venvs to central directory

--- dotfiles/functions/venv.fish
+++ dotfiles/functions/venv.fish
@@ -1,6 +1,6 @@
-# Defined in /tmp/fish.oBzqqP/venv.fish @ line 2
+# Defined in /tmp/fish.uoksJ1/venv.fish @ line 2
 function venv --argument dir
-	test -z $dir; and set -l dir .venv
+	test -z $dir; and set -l dir ~/.local/share/venvs/(pwd)
 	if test ! -d $dir
 		# don't base our new venv off of an existing one
 		functions -q deactivate; and deactivate
