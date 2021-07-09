e738d7db2c548d2156051129b49ebdb64fec33d2
io <gie9ohbeixah@paperboats.net>
2021-07-09 07:05:06 +0000

    update addgh.fish

--- dotfiles/functions/addgh.fish
+++ dotfiles/functions/addgh.fish
@@ -1,7 +1,8 @@
-# Defined in /tmp/fish.8TLHMK/addfork.fish @ line 2
 function addgh --argument name
 	test -z $name; and set -l name fork
 	set dir (basename (readlink -f .))
-	git remote add $name https://github.com/iomintz/$dir.git
-	git remote set-url --push $name git@github.com:iomintz/$dir.git
+	git remote add $name https://github.com/ioistired/$dir.git
+	git remote set-url --push $name git@github.com:ioistired/$dir.git
+	git config user.name io
+	git config user.email gie9ohbeixah@paperboats.net
 end
