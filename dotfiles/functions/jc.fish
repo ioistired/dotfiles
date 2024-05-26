14cab153e1daa256bdc3aa68550b53f4d974e9b0
ioistired <gie9ohbeixah@paperboats.net>
2024-05-26 01:11:33 +0000

    add systemctl and journalctl aliases

---
+++ dotfiles/functions/jc.fish
@@ -0,0 +1,4 @@
+function jc --wraps=journalctl --description 'alias jc journalctl'
+  journalctl $argv
+        
+end
---
+++ dotfiles/functions/sc.fish
@@ -0,0 +1,4 @@
+function sc --wraps=systemctl --description 'alias sc systemctl'
+  systemctl $argv
+        
+end
