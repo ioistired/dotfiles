9250a959002270464a459a0148af8bdefafced7e
ioistired <gie9ohbeixah@paperboats.net>
2024-05-26 01:08:56 +0000

    add $pipestatus to prompt

--- dotfiles/functions/fish_prompt.fish
+++ dotfiles/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
-# Defined in /tmp/fish.yJf3Tf/fish_prompt.fish @ line 2
 function fish_prompt --description 'Write out the prompt'
+	set -l last_pipestatus $pipestatus
 	set -l last_status $status
 
     # PWD
@@ -9,11 +9,13 @@ function fish_prompt --description 'Write out the prompt'
 
     __terlar_git_prompt
     __fish_hg_prompt
-    echo
 
     if not test $last_status -eq 0
         set_color $fish_color_error
+        echo -n ' ['(string join '|' $last_pipestatus)']'
     end
+
+    echo
 
     echo -n '➤ '
     set_color normal
