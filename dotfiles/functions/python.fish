245e97d3fae11d0702183e5e4d015d17f9b24471
ioistired <gie9ohbeixah@paperboats.net>
2025-12-29 20:48:41 +0000

    update python alias for mac

--- dotfiles/functions/python.fish
+++ dotfiles/functions/python.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 0
-function python --description 'alias python python -q'
-	command python -q $argv;
+function python --description 'alias python python3 -q'
+	command python3 -q $argv;
 end
